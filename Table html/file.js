746d2bb83eca0b5ddababc8cb38f36f34b9c1069
oladee <56429751+oladee@users.noreply.github.com>
2022-07-04 19:03:25 +0100

    Add files via upload

---
+++ Table html/file.js
@@ -0,0 +1,180 @@
+// data for the first person on the table
+const firstPerson = 
+{no:"1.",
+firstName:"Thompson",
+lastName:"Anthony",
+age:32,
+sex:"Male",
+position:"Head of Finance Department",
+dateOfEmployment:"17/07/2014"
+}
+// printing out with Javascript DOM
+document.getElementById("a1").innerHTML = firstPerson.no;
+document.getElementById("a2").innerHTML = firstPerson.firstName;
+document.getElementById("a3").innerHTML = firstPerson.lastName;
+document.getElementById("a4").innerHTML = firstPerson.age;
+document.getElementById("a5").innerHTML = firstPerson.sex;
+document.getElementById("a6").innerHTML = firstPerson.position;
+document.getElementById("a7").innerHTML = firstPerson.dateOfEmployment;
+
+const secondPerson = 
+{no: "2.",
+firstName:"Johnson",
+lastName:"Mary",
+age:23,
+sex:"Female",
+position:"Software Engineer",
+dateOfEmployment:"05/03/2019"
+}
+// Printing out with javascript DOM
+document.getElementById("b1").innerHTML = secondPerson.no;
+document.getElementById("b2").innerHTML = secondPerson.firstName;
+document.getElementById("b3").innerHTML = secondPerson.lastName;
+document.getElementById("b4").innerHTML = secondPerson.age;
+document.getElementById("b5").innerHTML = secondPerson.sex;
+document.getElementById("b6").innerHTML = secondPerson.position;
+document.getElementById("b7").innerHTML = secondPerson.dateOfEmployment;
+
+// Data for third person
+const thirdPerson = 
+{no:"3.",
+firstName:"Chisom",
+lastName:"Onyeka",
+age:28,
+sex:"Female",
+position:"Human Resource Manager",
+dateOfEmployment:"21/06/2016"
+}
+// Printing out data using DOM
+document.getElementById("c1").innerHTML = thirdPerson.no;
+document.getElementById("c2").innerHTML = thirdPerson.firstName;
+document.getElementById("c3").innerHTML = thirdPerson.lastName;
+document.getElementById("c4").innerHTML = thirdPerson.age;
+document.getElementById("c5").innerHTML = thirdPerson.sex;
+document.getElementById("c6").innerHTML = thirdPerson.position;
+document.getElementById("c7").innerHTML = thirdPerson.dateOfEmployment;
+// Data for fourth person
+const fourthPerson = 
+{no:"4.",
+firstName:"Jide",
+lastName:"Olabisi",
+age:29,
+sex:"Male",
+position:"Assistant Head (Software Department)",
+dateOfEmployment:"05/06/2016"
+}
+// printing data using DOM
+document.getElementById("d1").innerHTML = fourthPerson.no;
+document.getElementById("d2").innerHTML = fourthPerson.firstName;
+document.getElementById("d3").innerHTML = fourthPerson.lastName;
+document.getElementById("d4").innerHTML = fourthPerson.age;
+document.getElementById("d5").innerHTML = fourthPerson.sex;
+document.getElementById("d6").innerHTML = fourthPerson.position;
+document.getElementById("d7").innerHTML = fourthPerson.dateOfEmployment;
+// Data for fifth person
+const fifthPerson = 
+{no:"5.",
+firstName:"Williams",
+lastName:"Ajayi",
+age:30,
+sex:"Male",
+position:"Head of Software Department",
+dateOfEmployment:"06/09/2015"
+}
+// printing data using DOM
+document.getElementById("e1").innerHTML = fifthPerson.no;
+document.getElementById("e2").innerHTML = fifthPerson.firstName;
+document.getElementById("e3").innerHTML = fifthPerson.lastName;
+document.getElementById("e4").innerHTML = fifthPerson.age;
+document.getElementById("e5").innerHTML = fifthPerson.sex;
+document.getElementById("e6").innerHTML = fifthPerson.position;
+document.getElementById("e7").innerHTML = fifthPerson.dateOfEmployment;
+// Data for sixth person
+const sixthPerson = 
+{no:"6.",
+firstName:"Nancy",
+lastName:"Chukwuemeka",
+age:27,
+sex:"Female",
+position:"Secretary",
+dateOfEmployment:"23/08/2016"
+}
+// printing data using DOM
+document.getElementById("f1").innerHTML = sixthPerson.no;
+document.getElementById("f2").innerHTML = sixthPerson.firstName;
+document.getElementById("f3").innerHTML = sixthPerson.lastName;
+document.getElementById("f4").innerHTML = sixthPerson.age;
+document.getElementById("f5").innerHTML = sixthPerson.sex;
+document.getElementById("f6").innerHTML = sixthPerson.position;
+document.getElementById("f7").innerHTML = sixthPerson.dateOfEmployment;
+// Data for Seventh person
+const seventhPerson = 
+{no:"7.",
+firstName:"Nicholas",
+lastName:"Uchemba",
+age:26,
+sex:"Male",
+position:"Software Engineer",
+dateOfEmployment:"22/02/2020"}
+// printing data using DOM
+document.getElementById("g1").innerHTML = seventhPerson.no;
+document.getElementById("g2").innerHTML = seventhPerson.firstName;
+document.getElementById("g3").innerHTML = seventhPerson.lastName;
+document.getElementById("g4").innerHTML = seventhPerson.age;
+document.getElementById("g5").innerHTML = seventhPerson.sex;
+document.getElementById("g6").innerHTML = seventhPerson.position;
+document.getElementById("g7").innerHTML = seventhPerson.dateOfEmployment;
+
+// Data for eighth person
+const eighthPerson = 
+{no:"8.",
+firstName:"Ahmed",
+lastName:"Olayinka",
+age:24,
+sex:"Male",
+position:"Graphic Designer",
+dateOfEmployment:"15/08/2020"}
+// printing data using DOM
+document.getElementById("h1").innerHTML = eighthPerson.no;
+document.getElementById("h2").innerHTML = eighthPerson.firstName;
+document.getElementById("h3").innerHTML = eighthPerson.lastName;
+document.getElementById("h4").innerHTML = eighthPerson.age;
+document.getElementById("h5").innerHTML = eighthPerson.sex;
+document.getElementById("h6").innerHTML = eighthPerson.position;
+document.getElementById("h7").innerHTML = eighthPerson.dateOfEmployment;
+
+// Data for ninth person
+const ninthPerson = 
+{no:"9.",
+firstName:"Halima",
+lastName:"Mohammed",
+age:26,
+sex:"Female",
+position:"Senior Finance Manager",
+dateOfEmployment:"30/05/2018"}
+// printing data using DOM
+document.getElementById("i1").innerHTML = ninthPerson.no;
+document.getElementById("i2").innerHTML = ninthPerson.firstName;
+document.getElementById("i3").innerHTML = ninthPerson.lastName;
+document.getElementById("i4").innerHTML = ninthPerson.age;
+document.getElementById("i5").innerHTML = ninthPerson.sex;
+document.getElementById("i6").innerHTML = ninthPerson.position;
+document.getElementById("i7").innerHTML = ninthPerson.dateOfEmployment;
+
+// Data for Seventh person
+const tenthPerson = 
+{no:"10.",
+firstName:"Richard",
+lastName:"Adeyemi",
+age:41,
+sex:"Male",
+position:"C.E.O",
+dateOfEmployment:"17/11/2010"}
+// printing data using DOM
+document.getElementById("j1").innerHTML = tenthPerson.no;
+document.getElementById("j2").innerHTML = tenthPerson.firstName;
+document.getElementById("j3").innerHTML = tenthPerson.lastName;
+document.getElementById("j4").innerHTML = tenthPerson.age;
+document.getElementById("j5").innerHTML = tenthPerson.sex;
+document.getElementById("j6").innerHTML = tenthPerson.position;
+document.getElementById("j7").innerHTML = tenthPerson.dateOfEmployment;
